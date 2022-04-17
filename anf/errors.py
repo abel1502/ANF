2e30e20229d5ee6401d195caf0f5be4932d655a9
abel1502 <terragame.abel@gmail.com>
2022-04-17 23:05:40 +0300

    Enhance errors

--- anf/errors.py
+++ anf/errors.py
@@ -1,14 +1,52 @@
 import typing
 
 
-class StreamOpenError(Exception): pass
-class StreamReadError(Exception): pass
-class StreamWriteError(Exception): pass
+#################
+# Stream errors #
+#################
+
+class StreamOpenError(Exception):
+    pass
 
 
-class PacketEncodeError(Exception): pass
-class PacketDecodeError(Exception): pass
-class NotSizeableError(Exception): pass
-class PacketInvalidError(Exception): pass
+class StreamReadError(Exception):
+    pass
+
+
+class StreamWriteError(Exception):
+    pass
+
+
+#################
+# Packet errors #
+#################
+
+class PacketInvalidError(Exception):
+    pass
+
+
+class PacketEncodeError(PacketInvalidError):
+    pass
+
+
+class PacketObjTypeError(PacketEncodeError):
+    def __init__(self, obj: typing.Any, expected: typing.Tuple[typing.Type] | typing.Type):
+        super().__init__(f"Wrong packet obj type: got {type(obj)}, expected {expected}")
+
+    @classmethod
+    def validate(cls, obj: typing.Any, expected: typing.Tuple[typing.Type] | typing.Type | None):
+        if expected is None:
+            expected = type(None)
+
+        if not isinstance(obj, expected):
+            raise cls(obj, expected)
+
+
+class PacketDecodeError(PacketInvalidError):
+    pass
+
+
+class NotSizeableError(Exception):
+    pass
 
 
