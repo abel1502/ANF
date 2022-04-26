968f5f7c8e91f935d18b292104f13fd964f031b9
abel1502 <terragame.abel@gmail.com>
2022-04-26 15:19:05 +0300

    Refactor SizePrefixed
    This is what I needed `postponed` for

--- anf/packet/tunneling.py
+++ anf/packet/tunneling.py
@@ -78,45 +78,12 @@ class CountPrefixed(StructAdapter[ST]):
         ))
 
 
-# TODO: Encapsulate in some sort of restreamed, perhaps?
-class SizePrefixed(IPacket[T]):
-    def __init__(self, count_field: IPacket[int], data_field: IPacket[T]):
-        self.size_field: IPacket[int] = count_field.renamed("size")
-        self.data_field: IPacket[T] = data_field.renamed("data")
-
-    async def _encode(self, stream: IStream, obj: T, ctx: Context) -> None:
-        size_field = self.size_field
-        data_field = self.data_field
-
-        size_ctx = ctx.make_child(size_field.name)
-        data_ctx = ctx.make_child(data_field.name)
-
-        sub_stream = BytesStream()
-        await data_field.encode(sub_stream, obj, data_ctx)
-
-        enc = sub_stream.get_data()
-
-        await size_field.encode(stream, len(enc), size_ctx)
-        await stream.send(enc)
-
-    async def _decode(self, stream: IStream, ctx: Context) -> T:
-        size_field = self.size_field
-        size_ctx = ctx.make_child(size_field.name)
-        obj_size = await size_field.decode(stream, size_ctx)
-
-        sub_stream = BytesStream(await stream.recv(obj_size))
-
-        data_field = self.data_field
-        data_ctx = ctx.make_child(data_field.name)
-        obj: T = await data_field.decode(sub_stream, data_ctx)
-
-        return obj
-
-    def _sizeof(self, ctx: Context) -> int:
-        size_field = self.size_field
-        size_ctx: Context = ctx.get_member(size_field.name)
-
-        return size_field.sizeof(size_ctx) + size_ctx.value
+class SizePrefixed(StructAdapter[T]):
+    def __init__(self, size_field: IPacket[int], data_field: IPacket[T]):
+        super().__init__(Struct(
+            "size" / postponed(Deduced(size_field, lambda ctx: len(ctx.parent.data.encoded))),
+            "data" / data_field
+        ))
 
 
 __all__ = (
