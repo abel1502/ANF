5b2c59cbf0f56ca7b8b43db77d70caa200884704
abel1502 <terragame.abel@gmail.com>
2022-04-27 13:36:54 +0300

    Change Checksum impl

--- anf/packet/tunneling.py
+++ anf/packet/tunneling.py
@@ -85,12 +85,62 @@ class SizePrefixed(StructAdapter[T]):
         ))
 
 
-# TODO: Imporve to ease work with the surrounding struct as a whole
 class Checksum(PacketWrapper[T | None]):
+    """
+    A special packet that computes and validates a checksum automatically.
+    Often demands being `postpone`d.
+
+    Note: see the note for `postpone_dec` in AutoPacket.__init__
+    """
+
     def __init__(self, wrapped: IPacket[T | None],
                  hash_func: typing.Callable[[bytes], T | None],
-                 data: CtxParam[bytes]):
-        super().__init__(Const(lambda ctx: hash_func(eval_ctx_param(data, ctx)), wrapped))
+                 data: CtxParam[bytes], *,
+                 validate: typing.Callable[[Context, T, T], bool] =
+                 (lambda ctx, value, hashed: value == hashed),
+                 compute: typing.Callable[[Context, T], bytes] =
+                 (lambda ctx, hashed: hashed)):
+
+        hashed = lambda ctx: hash_func(eval_ctx_param(data, ctx))
+
+        super().__init__(Deduced(
+            wrapped,
+            lambda ctx: compute(ctx, hashed(ctx))
+        ))
+
+        self._validate_func = lambda ctx: validate(ctx, ctx.value, hashed(ctx))
+
+        self._postpone_validation = False
+
+    def _validate(self, ctx: Context, computed_here: bool = False) -> None:
+        if not self._validate_func(ctx):
+            PacketEncodeError("The computed hashsum didn't pass the validation somehow"
+                              if computed_here else
+                              "Wrong checksum")
+
+    async def _encode(self, stream: IStream, obj: T, ctx: Context) -> None:
+        await super()._encode(stream, obj, ctx)
+
+        if self._postpone_validation:
+            on_finish: Event[[]] = ctx.parent.get_md("on_finish")
+            on_finish.add(lambda: self._validate(ctx, True))
+        else:
+            self._validate(ctx, True)
+
+    async def _decode(self, stream: IStream, ctx: Context) -> T:
+        result = await super()._decode(stream, ctx)
+
+        if self._postpone_validation:
+            on_finish: Event[[]] = ctx.parent.get_md("on_finish")
+            on_finish.add(lambda: self._validate(ctx))
+        else:
+            ctx.register_val(result)  # So that it's immediately available
+            self._validate(ctx)
+
+        return result
+
+    def _postpone(self, level: int) -> None:
+        self._postpone_validation = True
 
 
 __all__ = (
